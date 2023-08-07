b8260ac451c269566fb51d19ef95134b35a7df53
goptun <ramos.matheus@icloud.com>
2023-08-06 23:59:32 -0300

    [M1S10] Ex 2 - Criação componente Header

---
+++ vitrine-de-medicamentos/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+function Header() {
+  return (
+    <a className="header" href="#">
+      <img className="logo" src="logo.svg" />
+      <span>Farmácia Ramos</span>
+    </a>
+  );
+}
+
+export default Header;
